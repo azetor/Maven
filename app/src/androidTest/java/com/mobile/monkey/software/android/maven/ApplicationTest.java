a46fb5b41c7c2b07f3db47447c3734b4f9fa7ea8
azetor <adam.a.szymczak@gmail.com>
2015-10-07 21:20:04 +0200

    Initial commit

---
+++ app/src/androidTest/java/com/mobile/monkey/software/android/maven/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mobile.monkey.software.android.maven;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
